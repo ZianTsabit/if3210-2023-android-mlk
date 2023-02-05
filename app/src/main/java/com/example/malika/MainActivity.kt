db97c3b0491c2550d228e4a097d5cebbb23c36e1
TubesForLyfe <13520160@std.stei.itb.ac.id>
2023-02-05 17:57:02 +0700

    Add Navbar & Menu Fragment

--- app/src/main/java/com/example/malika/MainActivity.kt
+++ app/src/main/java/com/example/malika/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.malika
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    private val cameraFragment = CameraFragment()
+    private val mapFragment = MapFragment()
+    private val foodFragment = FoodFragment()
+    private val bucketFragment = BucketFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(cameraFragment)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.camera -> replaceFragment(cameraFragment)
+                R.id.map -> replaceFragment(mapFragment)
+                R.id.food -> replaceFragment(foodFragment)
+                R.id.bucket -> replaceFragment(bucketFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
